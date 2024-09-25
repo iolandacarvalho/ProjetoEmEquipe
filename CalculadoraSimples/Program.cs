1701740d91214dcd358b3b44bb230989f0fa40b6
Beatriz Serafim <serafimbzz@gmail.com>
2024-09-25 10:08:51 -0300

    Excluindo Log da Vitoria

---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Informe o primeiro número para que possa calcular: ");
+    double valor1 = double.Parse (Console.ReadLine());
+
+Console.Write("informe o segundo número para calcular: ");
+    double valor2 = double.Parse (Console.ReadLine());
+
+Console.Write("Qual operação deseja realizar, Multiplicação, divisão, adição ou subtração? Insira a inicial da operação.: ");
+string operacao = Console.ReadLine();
+
+switch(operacao) 
+{
+  case "+":
+    Console.WriteLine($"O resultado da adição é: {valor1 + valor2} ");
+    break;
+  case "/":
+    Console.WriteLine($"O resultado da divisão é: {valor1 / valor2} ");
+    break;
+    case "*":
+    Console.WriteLine($"O resultado da multiplicação é: {valor1 * valor2} ");
+    break;
+     case "-":
+    Console.WriteLine($"O resultado da subtração é: {valor1 - valor2} ");
+    break;
+  default:
+    Console.WriteLine("Operador inválido");
+    break;
+}
+
+
